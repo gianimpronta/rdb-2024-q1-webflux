ae7f42816538bff78f65acb907fcee9675e38914
gianimpronta <gian.impronta@gmail.com>
2024-02-09 09:45:23 -0300

    Adicionando arquivos criados usando o Spring Initializer

---
+++ src/test/java/dev/gianimpronta/crebito/CrebitoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gianimpronta.crebito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrebitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
