c5b9ba687b84f70e98a73a22879c0555351d4893
gianimpronta <gian.impronta@gmail.com>
2024-02-20 13:47:42 -0300

    Validações funcionando

--- src/test/java/dev/gianimpronta/crebito/CrebitoApplicationTests.java
+++ src/test/java/dev/gianimpronta/crebito/CrebitoApplicationTests.java
@@ -1,13 +1,20 @@
 package dev.gianimpronta.crebito;
 
+import dev.gianimpronta.crebito.controllers.ClienteController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CrebitoApplicationTests {
+    @Autowired
+    ClienteController clienteController;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertThat(clienteController).isNotNull();
+    }
 
 }
