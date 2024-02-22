08b89c4bbd921bb07594d8e17c8d1a42cef934d1
gianimpronta <gian.impronta@gmail.com>
2024-02-22 19:48:37 -0300

    Refatorar o tratamento de exceções em classes de aconselhamento
    Métodos simplificados de tratamento de exceções nas classes 'TransacaoInvalidaAdvice' e 'ValidationAdvice'. A importação ResponseEntity não utilizada foi removida e a anotação @ResponseBody agora foi omitida para melhorar a legibilidade do código. Modificado o status HttpResponse de BAD_REQUEST para UNPROCESSABLE_ENTITY para refletir de forma mais adequada o status da resposta.

--- src/main/java/dev/gianimpronta/crebito/advisors/ValidationAdvice.java
+++ src/main/java/dev/gianimpronta/crebito/advisors/ValidationAdvice.java
@@ -2,16 +2,16 @@ package dev.gianimpronta.crebito.advisors;
 
 import org.springframework.core.codec.DecodingException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.WebExchangeBindException;
 
 @RestControllerAdvice
 public class ValidationAdvice {
-    @ResponseBody
-    @ExceptionHandler({ MethodArgumentNotValidException.class, DecodingException.class, WebExchangeBindException.class })
+    @ExceptionHandler({ MethodArgumentNotValidException.class, DecodingException.class, WebExchangeBindException.class, HttpMessageNotReadableException.class })
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public void validationExceptionHandler(MethodArgumentNotValidException e) {
+    public void validationExceptionHandler() {
         // exception validator
     }
 }
