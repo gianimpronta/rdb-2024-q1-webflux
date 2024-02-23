9793ec6194cb9e55b9224402f3bed8ae3763d8aa
gianimpronta <gian.impronta@gmail.com>
2024-02-22 21:19:53 -0300

    Atualizando script de inicialização do pg e adicionando um arquivo de conf do pg

--- init.sql
+++ init.sql
@@ -1,15 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
-CREATE TABLE IF NOT EXISTS clientes
+CREATE UNLOGGED TABLE IF NOT EXISTS clientes
 (
     id BIGSERIAL NOT NULL,
     limite BIGINT NOT NULL,
+    saldo BIGINT NOT NULL,
     CONSTRAINT pk_clientes PRIMARY KEY (id)
 );
 create index clientes_id_idx
     on clientes (id);
 
-CREATE TABLE transacoes
+CREATE UNLOGGED TABLE transacoes
 (
     id BIGSERIAL NOT NULL,
     valor      BIGINT                      NOT NULL,
@@ -22,6 +23,9 @@ CREATE TABLE transacoes
 ALTER TABLE transacoes
     ADD CONSTRAINT FK_TRANSACOES_ON_CLIENTE FOREIGN KEY (cliente_id) REFERENCES clientes (id);
 
+
+truncate table transacoes;
+truncate table clientes;
 
 DO
 $$
